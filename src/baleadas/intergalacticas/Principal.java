3a69c72a0e7e5d91f6280eedb3d60ecb9e5462a2
jordimairena <jordim95@hotmail.com>
2015-11-10 00:48:23 -0600

    listas enlazadas

--- src/baleadas/intergalacticas/Principal.java
+++ src/baleadas/intergalacticas/Principal.java
@@ -1222,8 +1222,13 @@ public class Principal extends javax.swing.JFrame {
 
     private void jButton9MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton9MouseClicked
         // TODO add your handling code here:
+        PilaIngredientes p=new PilaIngredientes();
         String nom_ingrediente=this.txt_nom_ingredientes.getText();
         String desc_ingrediente=this.txta_descripcion_ingredientes.getText();
+        p.push(new Ingredientes(nom_ingrediente,desc_ingrediente));
+        p.imprimir();
+        
+        
     }//GEN-LAST:event_jButton9MouseClicked
 
     private void jButton5MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton5MouseClicked
