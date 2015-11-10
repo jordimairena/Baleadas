aec0734f6e2d3d00f8153732f1115d13590098dc
FacultyDevelopment <walther.carrasco@laureate.net>
2015-11-09 21:39:11 -0600

    pila

---
+++ src/baleadas/intergalacticas/PilaCocinero.java
@@ -0,0 +1,48 @@
+package baleadas.intergalacticas;
+
+import javax.swing.JOptionPane;
+
+public class PilaCocinero {
+    NodoCocinero head;
+    int tamaño;
+
+    public PilaCocinero() {
+        this.head = null;
+        this.tamaño = 0;
+    }
+    
+    public boolean estaVacio(){
+        return head == null;
+    }
+    
+    public void push(Cocineros c){
+        if (this.head == null) {
+            NodoCocinero n = new NodoCocinero(c);
+            head = n;
+        }else{
+            NodoCocinero n = new NodoCocinero(c);
+            n.setSiguiente(head);
+            this.head = n;
+        }
+        tamaño++;
+    }
+    
+    public NodoCocinero pop(int pos){
+        NodoCocinero eliminar = null;
+        if(estaVacio()) {
+            JOptionPane.showMessageDialog(null, "La lista se encuentra vacia");
+        }
+        if(tamaño == 1) {
+            eliminar = head;
+            head = null;
+            this.tamaño--;
+            return eliminar;
+        }
+        else {
+            NodoCocinero actual = head;
+            this.head = head.getSiguiente();
+            this.tamaño--;
+            return actual;
+        }  
+    }
+}
